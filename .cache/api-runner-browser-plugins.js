ac16d02fd77a72094fefb76f89ffb48c34789b95
Conner Luzier <connerluzier@protonmail.com>
2019-08-14 11:29:05 -0400

    main files

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,22 @@
+module.exports = [{
+      plugin: require('/Users/connerluzier/Desktop/cluzier.github.io/node_modules/gatsby-remark-images/gatsby-browser.js'),
+      options: {"plugins":[],"maxWidth":960,"withWebp":true,"ignoreFileExtensions":[]},
+    },{
+      plugin: require('/Users/connerluzier/Desktop/cluzier.github.io/node_modules/gatsby-remark-autolink-headers/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/connerluzier/Desktop/cluzier.github.io/node_modules/gatsby-plugin-netlify-cms/gatsby-browser.js'),
+      options: {"plugins":[],"modulePath":"/Users/connerluzier/Desktop/cluzier.github.io/src/cms/index.js"},
+    },{
+      plugin: require('/Users/connerluzier/Desktop/cluzier.github.io/node_modules/gatsby-plugin-google-gtag/gatsby-browser.js'),
+      options: {"plugins":[],"trackingIds":["UA-73379983-2"],"pluginConfig":{"head":true}},
+    },{
+      plugin: require('/Users/connerluzier/Desktop/cluzier.github.io/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/connerluzier/Desktop/cluzier.github.io/node_modules/gatsby-plugin-catch-links/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/connerluzier/Desktop/cluzier.github.io/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ config.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  url: 'https://cluzier.github.io',
+  pathPrefix: '/',
+  title: 'Blog by Conner Luzier',
+  subtitle: 'Web/Software Developer based in Orlando Florida, Im kinda famous on CodePen.',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-73379983-2',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    }
+  ],
+  author: {
+    name: 'Conner Luzier',
+    photo: '/photo.jpeg',
+    bio: 'Web/Software Developer based in Orlando Florida, Im kinda famous on CodePen.',
+    contacts: {
+      email: 'connerluzier@protonmail.com',
+      twitter: 'conner_luzier',
+      github: 'cluzier',
+    }
+  }
+};
